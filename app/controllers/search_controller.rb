9f3659151900e9e9c412e6b0649d7d0fae4b4106
Silviu D. Bojica <bojicas@gmail.com>
2008-11-03 15:31:47 +0400

    "will_paginate" is now implemented into a short demo and working.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,12 @@ class SearchController < ApplicationController
   skip_before_filter :login_required
 
   def index
+    @properties = Property.paginate(:page => params[:page], :per_page => 5, :order => 'created_at DESC')
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @properties }
+    end
   end
 
 end
