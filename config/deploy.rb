638535f1a7810ac00a93bd4ea111f9557567b6c1
Silviu D. Bojica <bojicas@gmail.com>
2008-11-06 15:13:59 +0400

    Transfer mysql config data into config files (#38)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,16 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "estate"
+set :repository,  "/Users/bojicas/workspace/rubyspace/businessspace/estate"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/home/dineinua/rails/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
+set :scm, :git
+
+set :user, "dineinua"
 
 role :app, "your app-server here"
 role :web, "your web-server here"
