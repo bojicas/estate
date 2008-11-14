307ba79459a5c70f96ce60d2498549fd4ba227e1
Silviu D. Bojica <bojicas@me.com>
2008-11-14 15:34:03 +0400

    Add drop down menu in buildings (#52)

--- app/models/building.rb
+++ app/models/building.rb
@@ -2,6 +2,9 @@ class Building < ActiveRecord::Base
   belongs_to :project
   has_many :properties
   
+  validates_presence_of :project_id, :name
+  validates_numericality_of :project_id
+  
   # find related project
   def project
     project = Project.find(
@@ -9,5 +12,13 @@ class Building < ActiveRecord::Base
       :include => [:neighborhood => :emirate]
     )
     project
+  end
+  
+  def find_all_projects(neighborhood_id = nil)
+    if neighborhood_id
+      Project.find(:all, :conditions => ["neighborhood_id = ?", neighborhood_id]).map {|u| ["#{u.name}", u.id]}
+    else
+      Project.find(:all).map {|u| ["#{u.name}", u.id]}
+    end
   end
 end
