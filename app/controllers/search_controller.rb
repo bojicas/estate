a510bad3332d8264c7a8fae3d67f84234aff7597
Silviu D. Bojica <bojicas@me.com>
2008-11-15 23:40:42 +0400

    Simple search - create form (#9) - now also keeps the selection between calls

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,14 +7,20 @@ class SearchController < ApplicationController
     @property_types = Type.find(:all, :order => "name").map {|u| ["#{u.name}", u.id]}
      
     #session[:param_type_id] ||= String.new
-    #session[:param_location] ||= String.new
+    session[:param_location] ||= String.new
     
     session[:param_type_id] = params[:type_id] if params[:type_id]
     session[:param_location] = params[:location] if params[:location]
     
+    if session[:param_location].empty?
+      conditions = ["type_id = ?", params[:type_id]]   
+    else
+      conditions = ["type_id = ? and (emirates.name LIKE ? or neighborhoods.name LIKE ? or projects.name LIKE ? or buildings.name LIKE ?)", params[:type_id], params[:location], params[:location], params[:location], params[:location]] 
+    end
+    
     @properties = Property.paginate(:page => params[:page], 
       :include => [:building => [:project => [:neighborhood => :emirate]]], 
-      :conditions => ["type_id = ? and (emirates.name LIKE ? or neighborhoods.name LIKE ? or projects.name LIKE ? or buildings.name LIKE ?)", params[:type_id], params[:location], params[:location], params[:location], params[:location]],
+      :conditions => conditions,
       :per_page => 5, 
       :order => 'properties.created_at DESC')
     
