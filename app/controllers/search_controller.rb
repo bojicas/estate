5a3fb23628d8f0f496e3f30018cb6531ddd0d096
Silviu D. Bojica <bojicas@me.com>
2008-11-17 16:27:01 +0400

    Fixed will_paginate bug (#65).

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -13,15 +13,16 @@ class SearchController < ApplicationController
     session[:param_location] = params[:location] if params[:location]
     
     if session[:param_location].empty?
-      conditions = ["type_id = ?", params[:type_id]]   
+      conditions = ["type_id = ?", session[:param_type_id]]   
     else
-      conditions = ["type_id = ? and (emirates.name LIKE ? or neighborhoods.name LIKE ? or projects.name LIKE ? or buildings.name LIKE ?)", params[:type_id], params[:location], params[:location], params[:location], params[:location]] 
+      conditions = ["type_id = ? and (emirates.name LIKE ? or neighborhoods.name LIKE ? or projects.name LIKE ? or buildings.name LIKE ?)", session[:param_type_id], session[:param_location], session[:param_location], session[:param_location], session[:param_location]] 
     end
     
-    @properties = Property.paginate(:page => params[:page], 
+    @properties = Property.paginate( 
       :include => [:building => [:project => [:neighborhood => :emirate]]], 
       :conditions => conditions,
-      :per_page => 5, 
+      :per_page => 3,
+      :page => params[:page], 
       :order => 'properties.created_at DESC')
     
     respond_to do |format|
